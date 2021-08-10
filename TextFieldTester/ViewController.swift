43dd3083055c51f5d5a433371fba24241479cd6f
Jason R Tibbetts <jason@tibbetts.net>
2021-08-10 10:39:27 -0400

    First version.

--- TextFieldTester/ViewController.swift
+++ TextFieldTester/ViewController.swift
@@ -1,19 +1,22 @@
-//
-//  ViewController.swift
-//  TextFieldTester
-//
-//  Created by Tibbetts, Jason on 8/10/21.
-//
+//  Created by nrith on 8/10/21.
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var field1: UITextField!
+
+    @IBOutlet weak var field2: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func submit(_ sender: Any) {
+        let actionVC = UIAlertController(title: "You did it!", message: "\(field1.text ?? ""), \(field2.text ?? "")", preferredStyle: .alert)
+        actionVC.addAction(UIAlertAction(title: "Awesome!", style: .default, handler: nil))
+        present(actionVC, animated: true, completion: nil)
+    }
 
 }
 
