4ea8a97a93c233f71e47988f155a645b241f1502
Jason R Tibbetts <jason@tibbetts.net>
2021-08-10 14:50:16 -0400

    Cleaned up some stuff for posting on StackOverflow.

--- TextFieldTester/ViewController.swift
+++ TextFieldTester/ViewController.swift
@@ -5,15 +5,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var field1: UITextField!
-
     @IBOutlet weak var field2: UITextField!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
     @IBAction func submit(_ sender: Any) {
-        let actionVC = UIAlertController(title: "You did it!", message: "\(field1.text ?? ""), \(field2.text ?? "")", preferredStyle: .alert)
+        let actionVC = UIAlertController(title: "You did it!",
+                                         message: "\(field1.text ?? ""), \(field2.text ?? "")",
+                                         preferredStyle: .alert)
         actionVC.addAction(UIAlertAction(title: "Awesome!", style: .default, handler: nil))
         present(actionVC, animated: true, completion: nil)
     }
--- TextFieldTesterUITests/TextFieldTesterUITests.swift
+++ TextFieldTesterUITests/TextFieldTesterUITests.swift
@@ -10,15 +10,15 @@ class TextFieldTesterUITests: XCTestCase {
 
         let field1TextField = app.textFields["Field 1"]
         field1TextField.tap()
-        app/*@START_MENU_TOKEN@*/.buttons["Clear text"]/*[[".textFields[\"Field 1\"].buttons[\"Clear text\"]",".buttons[\"Clear text\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.buttons["Clear text"].tap()
         field1TextField.typeText("Oh")
 
         let field2TextField = app.textFields["Field 2"]
         field2TextField.tap()
-        app/*@START_MENU_TOKEN@*/.buttons["Clear text"]/*[[".textFields[\"Field 2\"].buttons[\"Clear text\"]",".buttons[\"Clear text\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.buttons["Clear text"].tap()
         field2TextField.typeText("Susanna")
 
-        app/*@START_MENU_TOKEN@*/.staticTexts["Button"]/*[[".buttons[\"Button\"].staticTexts[\"Button\"]",".staticTexts[\"Button\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.staticTexts["Button"].tap()
 
         app.alerts["You did it!"].scrollViews.otherElements.buttons["Awesome!"].tap()
     }
